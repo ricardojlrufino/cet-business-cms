2be4ace9c0fd78d7bbed9bfb69dda379cba74629
ricardojlrufino <ricardo.jl.rufino@gmail.com>
2013-11-27 20:46:13 -0200

    Criado estrutura de pastas e adicionado Documentação

---
+++ src/main/java/ApplicationManager.java
@@ -0,0 +1,29 @@
+
+public class ApplicationManager extends org.mentawai.core.ApplicationManager {
+	
+	
+	@Override
+	public void loadFilters() {
+		
+	}
+		
+	@Override
+	public void loadActions() {
+
+//		action(ClienteAction.class, "cadastro")
+//			.on(SUCCESS, fwd("/jsp/Cliente/cadastro.jsp"))
+//			.on(ERROR, fwd("/jsp/Cliente/cadastro.jsp"));
+//		
+//		action(ClienteAction.class, "salvar")
+//			.filter(new ClienteValidator())
+//			.on(SUCCESS, fwd("/jsp/Cliente/detalhes.jsp"))
+//			.on(ERROR, fwd("/jsp/Cliente/cadastro.jsp"));
+		
+	}
+	
+	
+	@Override
+	public void loadFormatters() {
+	}
+	
+}
